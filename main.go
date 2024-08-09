3b2da5b7419897d6b98014b4ee9cd63fadae15b2
Milos Folic <milos.folic@thingsolver.com>
2024-08-09 12:29:12 +0200

    Add all interfaces by default (#1)
    Signed-off-by: milosfolic <milos.folic@thingsolver.com>

--- main.go
+++ main.go
@@ -15,7 +15,6 @@ import (
 )
 
 var (
-	iface        string
 	protocol     string
 	saveToFile   string
 	displayDNS   bool
@@ -24,6 +23,7 @@ var (
 	packetRows   [][]interface{}
 	packetLimit  int
 	updatePeriod time.Duration
+	ifaces       []string
 )
 
 func main() {
@@ -32,17 +32,45 @@ func main() {
 		Short: "Network Analyzer Tool",
 		Run: func(cmd *cobra.Command, args []string) {
 			setupTable()
-			analyze(iface, protocol, saveToFile, displayDNS, displayHTTP)
+
+			var interfaces []pcap.Interface
+			var err error
+
+			if len(ifaces) == 0 {
+				// If no interfaces are specified, get all available interfaces
+				interfaces, err = pcap.FindAllDevs()
+				if err != nil {
+					log.Fatal(err)
+				}
+			} else {
+				// Use the specified interfaces
+				interfaces, err = getSpecifiedInterfaces(ifaces)
+				if err != nil {
+					log.Fatal(err)
+				}
+			}
+
+			for _, iface := range interfaces {
+				go analyze(iface.Name, protocol, saveToFile, displayDNS, displayHTTP)
+			}
+
+			// Create a ticker to update the table at the specified interval
+			ticker := time.NewTicker(updatePeriod)
+			defer ticker.Stop()
+
+			for range ticker.C {
+				updateTable()
+			}
 		},
 	}
 
-	rootCmd.Flags().StringVarP(&iface, "interface", "i", "en0", "Network interface to listen on")
 	rootCmd.Flags().StringVarP(&protocol, "protocol", "p", "all", "Protocol to filter (all, tcp, udp)")
 	rootCmd.Flags().StringVarP(&saveToFile, "save", "s", "", "File to save captured packets (PCAP format)")
-	rootCmd.Flags().BoolVarP(&displayDNS, "dns", "d", false, "Display DNS queries")
+	rootCmd.Flags().BoolVarP(&displayDNS, "dns", "d", false, "Display DNS queries and responses")
 	rootCmd.Flags().BoolVarP(&displayHTTP, "http", "t", false, "Display HTTP requests")
 	rootCmd.Flags().IntVarP(&packetLimit, "limit", "l", 20, "Limit the number of displayed rows in the table")
 	rootCmd.Flags().DurationVarP(&updatePeriod, "update-period", "u", 5*time.Second, "Time period for updating the table")
+	rootCmd.Flags().StringSliceVarP(&ifaces, "interfaces", "i", nil, "List of network interfaces to capture from (comma-separated). It listens on all by default")
 
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
@@ -53,10 +81,32 @@ func setupTable() {
 	packetTable = table.NewWriter()
 	packetTable.SetOutputMirror(os.Stdout)
 	packetTable.AppendHeader(table.Row{
-		"Protocol", "Src IP", "Dst IP", "Src Port", "Dst Port", "DNS Query", "DNS Response", "HTTP Method", "HTTP URL", "HTTP Host",
+		"Interface", "Protocol", "Src IP", "Dst IP", "Src Port", "Dst Port", "DNS Query", "DNS Response", "HTTP Method", "HTTP URL", "HTTP Host",
 	})
 	packetTable.SetStyle(table.StyleRounded)
 	packetTable.Style().Options.SeparateRows = true
+}
+
+func getSpecifiedInterfaces(ifaceNames []string) ([]pcap.Interface, error) {
+	var interfaces []pcap.Interface
+	allInterfaces, err := pcap.FindAllDevs()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, iface := range allInterfaces {
+		for _, name := range ifaceNames {
+			if iface.Name == name {
+				interfaces = append(interfaces, iface)
+			}
+		}
+	}
+
+	if len(interfaces) == 0 {
+		return nil, fmt.Errorf("no matching interfaces found")
+	}
+
+	return interfaces, nil
 }
 
 func analyze(iface, protocol, _ string, displayDNS, displayHTTP bool) {
@@ -68,26 +118,18 @@ func analyze(iface, protocol, _ string, displayDNS, displayHTTP bool) {
 
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 
-	ticker := time.NewTicker(updatePeriod)
-	defer ticker.Stop()
+	for packet := range packetSource.Packets() {
+		if packet == nil {
+			continue
+		}
+		if protocol != "all" && !filterByProtocol(packet, protocol) {
+			continue
+		}
+		analyzePacket(packet, iface, displayDNS, displayHTTP)
 
-	for {
-		select {
-		case packet := <-packetSource.Packets():
-			if packet == nil {
-				continue
-			}
-			if protocol != "all" && !filterByProtocol(packet, protocol) {
-				continue
-			}
-			analyzePacket(packet, displayDNS, displayHTTP)
-
-			if len(packetRows) > packetLimit {
-				packetRows = packetRows[1:]
-			}
-
-		case <-ticker.C:
-			updateTable()
+		// Limit the number of displayed rows
+		if len(packetRows) > packetLimit {
+			packetRows = packetRows[1:]
 		}
 	}
 }
@@ -103,7 +145,7 @@ func filterByProtocol(packet gopacket.Packet, protocol string) bool {
 	}
 }
 
-func analyzePacket(packet gopacket.Packet, displayDNS, displayHTTP bool) {
+func analyzePacket(packet gopacket.Packet, iface string, displayDNS, displayHTTP bool) {
 	var srcIP, dstIP, srcPort, dstPort, protocol, dnsQuery, dnsResponse, httpMethod, httpURL, httpHost string
 
 	ipLayer := packet.Layer(layers.LayerTypeIPv4)
@@ -119,6 +161,10 @@ func analyzePacket(packet gopacket.Packet, displayDNS, displayHTTP bool) {
 		protocol = "TCP"
 		srcPort = fmt.Sprintf("%d", tcp.SrcPort)
 		dstPort = fmt.Sprintf("%d", tcp.DstPort)
+
+		if displayHTTP {
+			httpMethod, httpURL, httpHost = checkForHTTP(packet)
+		}
 	}
 
 	udpLayer := packet.Layer(layers.LayerTypeUDP)
@@ -127,18 +173,14 @@ func analyzePacket(packet gopacket.Packet, displayDNS, displayHTTP bool) {
 		protocol = "UDP"
 		srcPort = fmt.Sprintf("%d", udp.SrcPort)
 		dstPort = fmt.Sprintf("%d", udp.DstPort)
-	}
 
-	if displayDNS {
-		dnsQuery, dnsResponse = checkForDNS(packet)
-	}
-
-	if displayHTTP {
-		httpMethod, httpURL, httpHost = checkForHTTP(packet)
+		if displayDNS {
+			dnsQuery, dnsResponse = checkForDNS(packet)
+		}
 	}
 
 	packetRows = append(packetRows, []interface{}{
-		protocol, srcIP, dstIP, srcPort, dstPort, dnsQuery, dnsResponse, httpMethod, httpURL, httpHost,
+		iface, protocol, srcIP, dstIP, srcPort, dstPort, dnsQuery, dnsResponse, httpMethod, httpURL, httpHost,
 	})
 }
 
@@ -166,7 +208,9 @@ func checkForDNS(packet gopacket.Packet) (string, string) {
 
 	if dns.QR {
 		for _, answer := range dns.Answers {
-			dnsResponse = answer.IP.String()
+			if answer.Type == layers.DNSTypeA {
+				dnsResponse = answer.IP.String()
+			}
 		}
 	}
 
@@ -180,26 +224,24 @@ func checkForHTTP(packet gopacket.Packet) (string, string, string) {
 	}
 
 	tcp, _ := tcpLayer.(*layers.TCP)
-	if tcp.DstPort != 80 && tcp.DstPort != 8080 && tcp.SrcPort != 80 && tcp.SrcPort != 8080 {
-		return "", "", ""
-	}
-
 	payload := string(tcp.Payload)
-	if strings.Contains(payload, "HTTP") {
-		lines := strings.Split(payload, "\r\n")
-		if len(lines) > 0 {
-			requestLine := strings.Fields(lines[0])
-			if len(requestLine) >= 3 {
-				method := requestLine[0]
-				url := requestLine[1]
-				var host string
-				for _, line := range lines {
-					if strings.HasPrefix(line, "Host:") {
-						host = strings.TrimSpace(strings.TrimPrefix(line, "Host:"))
-						break
+	if len(payload) > 0 && tcp.DstPort == 80 {
+		if strings.HasPrefix(payload, "GET ") || strings.HasPrefix(payload, "POST ") || strings.HasPrefix(payload, "PUT ") || strings.HasPrefix(payload, "DELETE ") {
+			lines := strings.Split(payload, "\r\n")
+			if len(lines) > 0 {
+				requestLine := strings.Fields(lines[0])
+				if len(requestLine) >= 3 {
+					method := requestLine[0]
+					url := requestLine[1]
+					var host string
+					for _, line := range lines {
+						if strings.HasPrefix(line, "Host:") {
+							host = strings.TrimSpace(strings.TrimPrefix(line, "Host:"))
+							break
+						}
 					}
+					return method, url, host
 				}
-				return method, url, host
 			}
 		}
 	}
